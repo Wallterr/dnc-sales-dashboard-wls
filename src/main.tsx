2fdae8b9b53bfbfd0b85d4da8d3e17f042d4f589
Wallterr <walltersteve@gmail.com>
2025-08-22 19:18:22 -0300

    Feat material-ui-insert

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import ReactDom from 'react-dom/client'
 import App from './App.tsx'
-import { GlobalStyle, darkTheme } from './styles'
+import { lightTheme, GlobalStyle } from './styles'
 
 ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={lightTheme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
