ef5266638f44ff8e354ed87c63129b379d36499c
Wallterr <walltersteve@gmail.com>
2025-08-30 13:43:01 -0300

    Feat: Form component

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { ThemeProvider } from 'styled-components'
 import ReactDom from 'react-dom/client'
 import App from './App.tsx'
-import { lightTheme, GlobalStyle } from './styles'
+import { GlobalStyle, darkTheme } from './styles'
 
 ReactDom.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <GlobalStyle />
       <App />
     </ThemeProvider>
